d96ed3f5e42d18c1a2ee460ad9a5ed98ec6b8ca6
Brandon Haley <kyle7286@gmail.com>
2021-03-28 16:37:44 -0400

    Added route for /movies; added view for /movies

--- controllers/theaterRoutes.js
+++ controllers/theaterRoutes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const { Movie, User, Flag } = require("../models");
 // const withAuth = require("../utils/auth");
 const _ = require('underscore');
-const { route } = require("./api/movieRoutes");
 
 
 
@@ -15,10 +14,34 @@ router.get("/theater", async (req, res) => {
     }
 })
 
-// Individual Movie Data | When you click on a poster
-router.get("/movie/:title", async (req, res) => {
+// All Movies | 
+router.get("/movies/", async (req, res) => {
     try {
+        // Query all the data from a movie title
+        const movieData = await Movie.findAll({
+            attributes: ['title', 'poster']
+        });
 
+        // Serialize the movie data for rendering
+        // Serialize data so the template can read it
+        const movies = movieData.map((element) => element.get({ plain: true }));
+
+        console.log(movies);
+        // Render Movie Page and send it the data
+        res.render('movies', {
+            movies,
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e);
+    }
+})
+
+
+
+// Individual Movie Data | When you click on a poster
+router.get("/movies/:title", async (req, res) => {
+    try {
         // Query all the data from a movie title
         const movieData = await Movie.findAll({
             where: {
@@ -30,7 +53,7 @@ router.get("/movie/:title", async (req, res) => {
         const data = movieData[0].get({ plain: true });
 
         // Render Movie Page and send it the data
-        res.render('movie', {
+        res.render('movieinfo', {
             ...data,
         })
     } catch (e) {
