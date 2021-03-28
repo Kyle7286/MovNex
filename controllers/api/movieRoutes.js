c1e545d2f79bee012b9221b3b3c25be36d621fbd
Brandon Haley <kyle7286@gmail.com>
2021-03-28 15:00:35 -0400

    Completed Create/Update User Flag route

--- controllers/api/movieRoutes.js
+++ controllers/api/movieRoutes.js
@@ -4,9 +4,6 @@ const _ = require('underscore')
 
 router.get("/", async (req, res) => {
     try {
-        // session id
-
-
         // Query all the movie data
         const movieData = await Movie.findAll({
             attributes: ['id', 'rating', 'genre', 'trailer', 'poster',]
@@ -19,9 +16,48 @@ router.get("/", async (req, res) => {
         res.status(200).json(shuffle);
 
     } catch (e) {
-
+        res.status(500).json(e);
     }
 })
+
+// Create/Update FlagMovie Route | http://localhost:3001/api/movies/flag
+router.post('/flag', async (req, res) => {
+    try {
+        const queryUM = await Flag.findAll({
+            where: {
+                user_id: req.body.user_id,
+                movie_id: req.body.movie_id,
+            }
+        });
+        console.log(queryUM);
+
+        // Create movie is req doesnt exist already, else update it if it does
+        if (queryUM == "") {
+            console.log(`UM association not found! Will create one now...`);
+            const newFlag = await Flag.create({
+                user_id: req.body.user_id,
+                movie_id: req.body.movie_id,
+                flag: req.body.flag
+            });
+            res.status(200).json(newFlag)
+        } else {
+            console.log(`UM association found! Update a movie user association`);
+            const updateFlag = await Flag.update({
+                flag: req.body.flag
+            }, {
+                where: {
+                    user_id: req.body.user_id,
+                    movie_id: req.body.movie_id
+                },
+            });
+            res.status(200).json(updateFlag)
+        }
+    } catch (err) {
+        console.log(`Error: ${err}`);
+        res.status(400).json(err);
+    }
+});
+
 
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -3,23 +3,21 @@ const Movie = require('./Movie');
 const Flag = require('./Flag');
 
 User.belongsToMany(Movie, {
-    // Define the third table needed to store the foreign keys
+
     through: {
         model: Flag,
         unique: false
     },
-    // Define an alias for when data is retrieved
-    as: 'flagged_movies'
+    as: 'x'
+
 });
 
 Movie.belongsToMany(User, {
-    // Define the third table needed to store the foreign keys
     through: {
         model: Flag,
         unique: false
     },
-    // Define an alias for when data is retrieved
-    as: 'movie_users'
+    as: 'y'
 });
 
 module.exports = { User, Movie, Flag };
