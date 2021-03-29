c25d57fd949f0ab76ebb23f04a615f8834e319e8
Brandon Haley <kyle7286@gmail.com>
2021-03-29 16:11:44 -0400

    Styled /pass/saved/maybe pages; added delete button to each poster

--- controllers/api/movieRoutes.js
+++ controllers/api/movieRoutes.js
@@ -37,7 +37,7 @@ router.post('/flag', async (req, res) => {
         if (queryUM == "") {
             console.log(`UM association not found! Will create one now...`);
             const newFlag = await Flag.create({
-                user_id: req.body.user_id,
+                user_id: req.session.user_id,
                 movie_id: req.body.movie_id,
                 flag: req.body.flag
             });
